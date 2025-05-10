86c55556f0588843ea6c6b92d401644cb7889d1b
chavdar7 <furkanchavdar@gmail.com>
2025-05-10 17:01:29 +0300

    graph search h board size ve action number güncellendi

--- GRAPH_SEARCH.h
+++ GRAPH_SEARCH.h
@@ -4,11 +4,11 @@
 #include "data_types.h"
 
 // ==================== WRITE YOUR OPTIONAL COMMANDS ===========================
-#define CITY_NUMBER  20
+#define BOARD_SIZE 8 // or 16 or 32
 
 // ========  YOUR COMPULSORY (BUT SPECIFIC TO THE PROBLEM) COMMANDS ============ 
 #define PREDETERMINED_GOAL_STATE  1 // User will initially determine the goal state if it is true (1)
-#define ACTIONS_NUMBER  20          // The number of all possible actions
+#define ACTIONS_NUMBER  8        // The number of all possible actions
 #define MAX_SEARCHED_NODE 100000000 // exit from the search process if it is exceeded  
 
 // ================== YOU DO NOT NEED TO CHANGE THIS PART ======================
